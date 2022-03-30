c1afc373b169978fa8521cae2495aafefdb55993
Di Lena Rodriguez <diedilenarodriguez@FVFZMXV7J1WK.tld>
2022-03-30 14:09:42 -0500

    first commit

--- app.js
+++ app.js
@@ -7,10 +7,14 @@ function human() {
 human()
 
 function ran() {
-    var ran = Math.floor(Math.random() * 10) + 1
-    console.log(ran)
-    alert(`Now the program will generate a new number`)
-    alert(`The number is ${ran}`)
-    return ran 
+    var rand = Math.floor(Math.random() * 10) + 1
+    console.log(rand)
+    return rand
 }
 ran()
+
+function user() {
+    alert(`You got ${rand}`)
+    return user
+} 
+user()
